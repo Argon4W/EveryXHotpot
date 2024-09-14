53a7b9786e0823d48efb2265c6d00a50777a7853
Argon4W <1516775492@qq.com>
2024-09-15 03:59:40 +0800

    Extract ModelRenderer

--- src/main/java/com/github/argon4w/hotpot/client/blocks/ISectionGeometryBLockEntityRenderer.java
+++ src/main/java/com/github/argon4w/hotpot/client/blocks/ISectionGeometryBLockEntityRenderer.java
@@ -4,14 +4,15 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.client.event.AddSectionGeometryEvent;
 import net.neoforged.neoforge.client.model.data.ModelData;
 
 public interface ISectionGeometryBLockEntityRenderer {
     void renderSectionGeometry(AddSectionGeometryEvent.SectionRenderingContext context, PoseStack stack, BlockPos blockPos, ModelRenderer modelRenderer);
 
-    @FunctionalInterface
     interface ModelRenderer {
         void renderModel(BakedModel model, PoseStack stack, RenderType renderType, int overlay, ModelData modelData);
+        void renderModel(BakedModel model, BlockState blockState, PoseStack stack, RenderType renderType, int overlay, ModelData modelData);
     }
 }
--- src/main/java/com/github/argon4w/hotpot/client/events/HotpotClientGameEvents.java
+++ src/main/java/com/github/argon4w/hotpot/client/events/HotpotClientGameEvents.java
@@ -39,12 +39,11 @@ public class HotpotClientGameEvents {
     @SubscribeEvent
     public static void addSectionGeometry(AddSectionGeometryEvent event) {
         event.addRenderer(context -> {
-            BlockAndTintGetter region = context.getRegion();
             BlockPos startPos = event.getSectionOrigin();
             BlockPos endPos = startPos.offset(15, 15, 15);
 
             for (BlockPos pos : BlockPos.betweenClosed(startPos, endPos)) {
-                BlockEntity blockEntity = region.getBlockEntity(pos);
+                BlockEntity blockEntity = context.getRegion().getBlockEntity(pos);
 
                 if (blockEntity == null) {
                     continue;
@@ -57,10 +56,23 @@ public class HotpotClientGameEvents {
                 context.getPoseStack().pushPose();
                 context.getPoseStack().translate(pos.getX() - startPos.getX(), pos.getY() - startPos.getY(), pos.getZ() - startPos.getZ());
 
-                renderer.renderSectionGeometry(context, new PoseStack(), pos, (model, stack, renderType, overlay, modelData) -> LightPipelineAwareModelBlockRenderer.render(context.getOrCreateChunkBuffer(renderType), context.getQuadLighter(true), context.getRegion(), new TransformedBakedModel(model, stack), region.getBlockState(pos), pos, context.getPoseStack(), false, RANDOM_SOURCE, 42L, overlay, modelData, renderType));
+                renderer.renderSectionGeometry(context, new PoseStack(), pos, new SectionGeometryModelRenderer(context, pos));
                 context.getPoseStack().popPose();
             }
         });
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    public record SectionGeometryModelRenderer(AddSectionGeometryEvent.SectionRenderingContext context, BlockPos pos) implements ISectionGeometryBLockEntityRenderer.ModelRenderer {
+        @Override
+        public void renderModel(BakedModel model, PoseStack stack, RenderType renderType, int overlay, ModelData modelData) {
+            LightPipelineAwareModelBlockRenderer.render(context.getOrCreateChunkBuffer(renderType), context.getQuadLighter(true), context.getRegion(), new TransformedBakedModel(model, stack), context.getRegion().getBlockState(pos), pos, context.getPoseStack(), false, RANDOM_SOURCE, 42L, overlay, modelData, renderType);
+        }
+
+        @Override
+        public void renderModel(BakedModel model, BlockState blockState, PoseStack stack, RenderType renderType, int overlay, ModelData modelData) {
+            LightPipelineAwareModelBlockRenderer.render(context.getOrCreateChunkBuffer(renderType), context.getQuadLighter(true), context.getRegion(), new TransformedBakedModel(model, stack), blockState, pos, context.getPoseStack(), false, RANDOM_SOURCE, 42L, overlay, modelData, renderType);
+        }
     }
 
     public record TransformedBakedModel(BakedModel model, IQuadTransformer transformer) implements BakedModel {
