f7ddf8d343637a19cc263a0df22a775b831e4e0b
Argon4W <1516775492@qq.com>
2025-03-16 12:00:02 +0800

    Fix issue when installing with ModernFix; Fix texture rendering issue when installing with Sodium

--- src/main/java/com/github/argon4w/hotpot/mixins/sections/RenderTypeMixin.java
+++ src/main/java/com/github/argon4w/hotpot/mixins/sections/RenderTypeMixin.java
@@ -6,6 +6,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.RenderType;
 import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.LoadingModList;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -21,12 +22,12 @@ public class RenderTypeMixin {
             value = "FIELD",
             target = "Lnet/minecraft/client/renderer/RenderType;CHUNK_BUFFER_LAYERS:Lcom/google/common/collect/ImmutableList;"))
     private static void modifyChunkBufferLayers(ImmutableList<RenderType> value, Operation<Void> original) {
-        if (ModList.get().isLoaded("sodium")) {
+        if (LoadingModList.get().getModFileById("sodium") != null) {
             original.call(value);
             return;
         }
 
-        if (ModList.get().isLoaded("embeddium")) {
+        if (LoadingModList.get().getModFileById("embeddium") != null) {
             original.call(value);
             return;
         }
